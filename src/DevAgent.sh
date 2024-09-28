1c40ffac38f4cc8356ee71c93d18e9120b7c389a
Peter Hansen <mail@peterhansen.io>
2024-09-28 19:33:34 +0000

    Remove Unused executable check

--- src/DevAgent.sh
+++ src/DevAgent.sh
@@ -9,11 +9,6 @@ log "INFO: Argv = $@"
 declare CONTEXT="${BASH_SOURCE[0]}"; CONTEXT="$(realpath --strip "${CONTEXT%/*}/..")"
 ### Context is the Working Tree Root ###
 
-command -v direnv &>/dev/null || {
-  log 'ERROR: can not locate `direnv` on path'
-  exit 1
-}
-
 pushd "${CONTEXT}" &>/dev/null || {
   log "DevAgent Directory does not exist"
   exit 1
