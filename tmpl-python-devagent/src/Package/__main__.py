922a6d7c90135eee4f64df143f73f578c4eca6f7
Peter Hansen <mail@peterhansen.io>
2024-12-12 21:28:05 +0000

    Fix _get_kwarg helper

--- tmpl-python-devagent/src/Package/__main__.py
+++ tmpl-python-devagent/src/Package/__main__.py
@@ -26,8 +26,8 @@ def main(
     try: return args.popleft()
     except IndexError: raise E(f'missing positional arg: `{name.upper()}`')
   NO_DEFAULT = type('NO_DEFAULT', (), {})
-  def _get_kwarg(k: str, default: str | type[NO_DEFAULT] = NO_DEFAULT) -> str:
-    assert default is NO_DEFAULT or isinstance(default, str)
+  def _get_kwarg(k: str, default: str | bool | type[NO_DEFAULT] = NO_DEFAULT) -> str:
+    assert default is NO_DEFAULT or isinstance(default, (str, bool))
     try: return kwargs.get(k, default) if default is not NO_DEFAULT else kwargs[k]
     except KeyError: raise E(f'Missing Expected Flag: `--{k}`')
 
